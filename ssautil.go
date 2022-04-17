590265c690ec3cdd4a10e777d9b34d5a3a93fdc9
Shinnosuke Sawada <6warashi9@gmail.com>
2022-04-17 14:50:34 +0900

    feat: implement Referrers and Operands

---
+++ ssautil.go
@@ -0,0 +1,36 @@
+package ssautil
+
+import "golang.org/x/tools/go/ssa"
+
+type Referrerer interface {
+	Referrers() *[]ssa.Instruction
+}
+
+type Operander interface {
+	Operands([]*ssa.Value) []*ssa.Value
+}
+
+func Referrers(r Referrerer) []ssa.Instruction {
+	refs := r.Referrers()
+	if refs == nil {
+		return nil
+	}
+	nonnil := make([]ssa.Instruction, 0, len(*refs))
+	for i := range *refs {
+		if (*refs)[i] != nil {
+			nonnil = append(nonnil, (*refs)[i])
+		}
+	}
+	return nonnil
+}
+
+func Operands(o Operander) []ssa.Value {
+	ops := o.Operands(nil)
+	nonnil := make([]ssa.Value, 0, len(ops))
+	for i := range ops {
+		if ops[i] != nil && *ops[i] != nil {
+			nonnil = append(nonnil, *ops[i])
+		}
+	}
+	return nonnil
+}
