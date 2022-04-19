7798c5a27fcb3647cc75c2ec48b02c4d1786382b
Shinnosuke Sawada <6warashi9@gmail.com>
2022-04-19 09:04:21 +0900

    fix: trim space

--- file.go
+++ file.go
@@ -3,13 +3,14 @@ package ssautil
 import (
 	"go/ast"
 	"regexp"
+	"strings"
 )
 
 var GeneratedRegexp = regexp.MustCompile(`^Code generated .* DO NOT EDIT\.$`)
 
 func IsGenerated(f *ast.File) bool {
 	for _, c := range f.Comments {
-		if GeneratedRegexp.MatchString(c.Text()) {
+		if GeneratedRegexp.MatchString(strings.TrimSpace(c.Text())) {
 			return true
 		}
 	}
