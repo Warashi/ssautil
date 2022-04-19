b9f156810dc4cca9022dc505685bac4f67946c01
Shinnosuke Sawada <6warashi9@gmail.com>
2022-04-19 09:22:10 +0900

    fix: generated comments detection

--- file.go
+++ file.go
@@ -6,12 +6,14 @@ import (
 	"strings"
 )
 
-var GeneratedRegexp = regexp.MustCompile(`^Code generated .* DO NOT EDIT\.$`)
+var GeneratedRegexp = regexp.MustCompile(`^// Code generated .* DO NOT EDIT\.$`)
 
 func IsGenerated(f *ast.File) bool {
 	for _, c := range f.Comments {
-		if GeneratedRegexp.MatchString(strings.TrimSpace(c.Text())) {
-			return true
+		for _, c := range c.List {
+			if GeneratedRegexp.MatchString(strings.TrimSpace(c.Text)) {
+				return true
+			}
 		}
 	}
 	return false
